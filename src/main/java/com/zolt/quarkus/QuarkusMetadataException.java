8c0d53fc39d417dd972ad1b4e6b900b8d1bdf06a
zsumz <shawn@zsumz.com>
2026-06-08 12:08:11 -0500

    feat: read quarkus extension metadata

---
+++ src/main/java/com/zolt/quarkus/QuarkusMetadataException.java
@@ -0,0 +1,11 @@
+package com.zolt.quarkus;
+
+public final class QuarkusMetadataException extends RuntimeException {
+    public QuarkusMetadataException(String message) {
+        super(message);
+    }
+
+    public QuarkusMetadataException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
