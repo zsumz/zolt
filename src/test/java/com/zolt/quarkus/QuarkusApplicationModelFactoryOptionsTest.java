662bcb6e9edff536036a1e6bf65b1c659e3961bb
zsumz <shawn@zsumz.com>
2026-06-16 23:38:20 +0000

    refactor: split quarkus application model removed resource tests

--- src/test/java/com/zolt/quarkus/QuarkusApplicationModelFactoryOptionsTest.java
+++ src/test/java/com/zolt/quarkus/QuarkusApplicationModelFactoryOptionsTest.java
@@ -147,73 +147,7 @@ final class QuarkusApplicationModelFactoryOptionsTest {
                 model.parentFirstArtifacts());
     }
 
-    @Test
-    void addsRemovedResourcesFromRuntimeExtensionMetadata() throws IOException {
-        Path quarkusRest = tempDir.resolve("quarkus-rest.jar");
-        writeJar(
-                quarkusRest,
-                QuarkusExtensionMetadataReader.METADATA_PATH,
-                """
-                deployment-artifact=io.quarkus:quarkus-rest-deployment:3.33.2
-                removed-resources.io.quarkus\\:quarkus-rest=\
-                META-INF/services/io.quarkus.runtime.test.TestHttpEndpointProvider,\
-                META-INF/duplicate.index
-                """);
-        QuarkusApplicationModelFactory factory = new QuarkusApplicationModelFactory(fakeApiWithArtifactKey());
-
-        QuarkusApplicationModelHandle handle = factory.create(descriptor(
-                List.of(),
-                List.of(new QuarkusBootstrapDependency(
-                        new PackageId("io.quarkus", "quarkus-rest"),
-                        "3.33.2",
-                        DependencyScope.COMPILE,
-                        quarkusRest,
-                        true)),
-                List.of(quarkusRest),
-                List.of()));
-
-        FakeApplicationModel model = assertInstanceOf(FakeApplicationModel.class, handle.applicationModel());
-        assertEquals(
-                Map.of(
-                        new FakeArtifactKey(
-                                "io.quarkus",
-                                "quarkus-rest",
-                                "",
-                                "jar"),
-                        List.of(
-                                "META-INF/services/io.quarkus.runtime.test.TestHttpEndpointProvider",
-                                "META-INF/duplicate.index")),
-                model.removedResources());
-    }
-
-    @Test
-    void addsTestBootstrapRemovedResourceOption() {
-        QuarkusApplicationModelFactory factory = new QuarkusApplicationModelFactory(fakeApiWithArtifactKey());
-
-        QuarkusApplicationModelHandle handle = factory.create(
-                descriptor(),
-                java.util.Optional.empty(),
-                QuarkusApplicationModelOptions.TEST_BOOTSTRAP);
-
-        FakeApplicationModel model = assertInstanceOf(FakeApplicationModel.class, handle.applicationModel());
-        assertEquals(
-                Map.of(
-                        new FakeArtifactKey(
-                                "io.quarkus",
-                                "quarkus-rest",
-                                "",
-                                "jar"),
-                        List.of("META-INF/services/io.quarkus.runtime.test.TestHttpEndpointProvider"),
-                        new FakeArtifactKey(
-                                "io.quarkus",
-                                "quarkus-arc",
-                                "",
-                                "jar"),
-                        List.of("META-INF/services/io.quarkus.runtime.test.TestScopeSetup")),
-                model.removedResources());
-    }
-
-    private static QuarkusBootstrapDescriptor descriptor() {
+    static QuarkusBootstrapDescriptor descriptor() {
         return descriptor(
                 List.of(),
                 List.of(
@@ -233,7 +167,7 @@ final class QuarkusApplicationModelFactoryOptionsTest {
                 List.of(Path.of("/cache/quarkus-rest-deployment.jar")));
     }
 
-    private static QuarkusBootstrapDescriptor descriptor(
+    static QuarkusBootstrapDescriptor descriptor(
             List<Path> platformPropertiesFiles,
             List<QuarkusBootstrapDependency> bootstrapDependencies,
             List<Path> runtimeClasspath,
