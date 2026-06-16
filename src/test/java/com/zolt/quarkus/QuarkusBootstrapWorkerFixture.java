2a14f402fb0cf0890e5538688e975f319f5acfc2
zsumz <shawn@zsumz.com>
2026-06-16 22:34:21 +0000

    refactor: split quarkus bootstrap worker test doubles

--- src/test/java/com/zolt/quarkus/QuarkusBootstrapWorkerFixture.java
+++ src/test/java/com/zolt/quarkus/QuarkusBootstrapWorkerFixture.java
@@ -91,10 +91,10 @@ public final class QuarkusBootstrapWorkerFixture {
 
     private static QuarkusApplicationModelFactory modelFactory() {
         return new QuarkusApplicationModelFactory(new QuarkusApplicationModelApi(
-                FakeApplicationModelBuilder.class.getName(),
-                FakeResolvedDependencyBuilder.class.getName(),
-                FakePlatformImports.class.getName(),
-                FakePlatformImportsImpl.class.getName()));
+                QuarkusBootstrapWorkerTestDoubles.FakeApplicationModelBuilder.class.getName(),
+                QuarkusBootstrapWorkerTestDoubles.FakeResolvedDependencyBuilder.class.getName(),
+                QuarkusBootstrapWorkerTestDoubles.FakePlatformImports.class.getName(),
+                QuarkusBootstrapWorkerTestDoubles.FakePlatformImportsImpl.class.getName()));
     }
 
     public static final class WorkerBootstrap {
@@ -136,11 +136,11 @@ public final class QuarkusBootstrapWorkerFixture {
                 return this;
             }
 
-            public Builder setExistingModel(FakeApplicationModel ignored) {
+            public Builder setExistingModel(QuarkusBootstrapWorkerTestDoubles.FakeApplicationModel ignored) {
                 return this;
             }
 
-            public Builder addLocalArtifact(FakeArtifactKey ignored) {
+            public Builder addLocalArtifact(QuarkusBootstrapWorkerTestDoubles.FakeArtifactKey ignored) {
                 return this;
             }
 
@@ -225,87 +225,4 @@ public final class QuarkusBootstrapWorkerFixture {
         }
     }
 
-    public static final class FakeApplicationModelBuilder {
-        private FakeResolvedDependencyBuilder appArtifact;
-        private final java.util.List<FakeResolvedDependencyBuilder> dependencies = new java.util.ArrayList<>();
-
-        public FakeApplicationModelBuilder setAppArtifact(FakeResolvedDependencyBuilder appArtifact) {
-            this.appArtifact = appArtifact;
-            return this;
-        }
-
-        public FakeApplicationModelBuilder addDependency(FakeResolvedDependencyBuilder dependency) {
-            dependencies.add(dependency);
-            return this;
-        }
-
-        public FakeApplicationModel build() {
-            return new FakeApplicationModel(appArtifact, java.util.List.copyOf(dependencies));
-        }
-    }
-
-    public static final class FakeResolvedDependencyBuilder {
-        public static FakeResolvedDependencyBuilder newInstance() {
-            return new FakeResolvedDependencyBuilder();
-        }
-
-        public FakeResolvedDependencyBuilder setGroupId(String ignored) {
-            return this;
-        }
-
-        public FakeResolvedDependencyBuilder setArtifactId(String ignored) {
-            return this;
-        }
-
-        public FakeResolvedDependencyBuilder setVersion(String ignored) {
-            return this;
-        }
-
-        public FakeResolvedDependencyBuilder setClassifier(String ignored) {
-            return this;
-        }
-
-        public FakeResolvedDependencyBuilder setType(String ignored) {
-            return this;
-        }
-
-        public FakeResolvedDependencyBuilder setScope(String ignored) {
-            return this;
-        }
-
-        public FakeResolvedDependencyBuilder setResolvedPath(Path ignored) {
-            return this;
-        }
-
-        public FakeResolvedDependencyBuilder setDirect(boolean ignored) {
-            return this;
-        }
-
-        public FakeResolvedDependencyBuilder setRuntimeCp() {
-            return this;
-        }
-
-        public FakeResolvedDependencyBuilder setDeploymentCp() {
-            return this;
-        }
-    }
-
-    public interface FakePlatformImports {
-    }
-
-    public static final class FakePlatformImportsImpl implements FakePlatformImports {
-        public void setPlatformProperties(java.util.Map<String, String> ignored) {
-        }
-    }
-
-    public record FakeArtifactKey(String groupId, String artifactId, String classifier, String type) {
-        public static FakeArtifactKey of(String groupId, String artifactId, String classifier, String type) {
-            return new FakeArtifactKey(groupId, artifactId, classifier, type);
-        }
-    }
-
-    public record FakeApplicationModel(
-            FakeResolvedDependencyBuilder appArtifact,
-            java.util.List<FakeResolvedDependencyBuilder> dependencies) {
-    }
 }
