98a07073eb7de9ea6516cc0766ef1f398749eb3c
zsumz <shawn@zsumz.com>
2026-06-17 02:01:49 +0000

    refactor: split run package spring boot tests

---
+++ src/test/java/com/zolt/build/RunPackageServiceSpringBootTestSupport.java
@@ -0,0 +1,150 @@
+package com.zolt.build;
+
+import com.zolt.classpath.ClasspathBuilder;
+import com.zolt.doctor.JdkChecker;
+import com.zolt.doctor.JdkDetector;
+import com.zolt.project.BuildSettings;
+import com.zolt.project.PackageMode;
+import com.zolt.project.PackageSettings;
+import com.zolt.project.ProjectConfig;
+import com.zolt.project.ProjectConfigs;
+import com.zolt.project.ProjectMetadata;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+import java.util.Optional;
+import java.util.jar.JarEntry;
+import java.util.jar.JarOutputStream;
+
+abstract class RunPackageServiceSpringBootTestSupport {
+    protected static RunPackageService service(JavaRunner.ProcessRunner processRunner) {
+        return service(processRunner, new JdkDetector());
+    }
+
+    protected static RunPackageService service(JavaRunner.ProcessRunner processRunner, JdkChecker jdkChecker) {
+        return new RunPackageService(
+                new PackageService(),
+                new BuildService(jdkChecker),
+                new ClasspathBuilder(),
+                jdkChecker,
+                new JavaRunner(":", processRunner));
+    }
+
+    protected static void writeSpringBootLockfile(Path projectDir, Path cacheRoot) throws IOException {
+        createJarWithEntry(
+                cacheRoot.resolve("org/springframework/boot/spring-boot/4.0.6/spring-boot-4.0.6.jar"),
+                "org/springframework/boot/SpringApplication.class");
+        createJarWithEntry(
+                cacheRoot.resolve("org/springframework/boot/spring-boot-loader/4.0.6/spring-boot-loader-4.0.6.jar"),
+                "org/springframework/boot/loader/launch/JarLauncher.class");
+        createJarWithEntry(
+                cacheRoot.resolve("com/example/runtime-lib/1.0.0/runtime-lib-1.0.0.jar"),
+                "com/example/runtime/RuntimeLib.class");
+        Files.writeString(projectDir.resolve("zolt.lock"), """
+                version = 1
+
+                [[package]]
+                id = "org.springframework.boot:spring-boot"
+                version = "4.0.6"
+                source = "maven-central"
+                scope = "compile"
+                direct = false
+                jar = "org/springframework/boot/spring-boot/4.0.6/spring-boot-4.0.6.jar"
+                dependencies = []
+
+                [[package]]
+                id = "org.springframework.boot:spring-boot-loader"
+                version = "4.0.6"
+                source = "maven-central"
+                scope = "runtime"
+                direct = false
+                jar = "org/springframework/boot/spring-boot-loader/4.0.6/spring-boot-loader-4.0.6.jar"
+                dependencies = []
+
+                [[package]]
+                id = "com.example:runtime-lib"
+                version = "1.0.0"
+                source = "maven-central"
+                scope = "runtime"
+                direct = false
+                jar = "com/example/runtime-lib/1.0.0/runtime-lib-1.0.0.jar"
+                dependencies = []
+                """);
+    }
+
+    protected static void writeSpringBootWarLockfile(Path projectDir, Path cacheRoot) throws IOException {
+        createJarWithEntry(
+                cacheRoot.resolve("org/springframework/boot/spring-boot/4.0.6/spring-boot-4.0.6.jar"),
+                "org/springframework/boot/SpringApplication.class");
+        createJarWithEntry(
+                cacheRoot.resolve("org/springframework/boot/spring-boot-loader/4.0.6/spring-boot-loader-4.0.6.jar"),
+                "org/springframework/boot/loader/launch/WarLauncher.class");
+        createJarWithEntry(
+                cacheRoot.resolve("com/example/runtime-lib/1.0.0/runtime-lib-1.0.0.jar"),
+                "com/example/runtime/RuntimeLib.class");
+        Files.writeString(projectDir.resolve("zolt.lock"), """
+                version = 1
+
+                [[package]]
+                id = "org.springframework.boot:spring-boot"
+                version = "4.0.6"
+                source = "maven-central"
+                scope = "compile"
+                direct = false
+                jar = "org/springframework/boot/spring-boot/4.0.6/spring-boot-4.0.6.jar"
+                dependencies = []
+
+                [[package]]
+                id = "org.springframework.boot:spring-boot-loader"
+                version = "4.0.6"
+                source = "maven-central"
+                scope = "runtime"
+                direct = false
+                jar = "org/springframework/boot/spring-boot-loader/4.0.6/spring-boot-loader-4.0.6.jar"
+                dependencies = []
+
+                [[package]]
+                id = "com.example:runtime-lib"
+                version = "1.0.0"
+                source = "maven-central"
+                scope = "runtime"
+                direct = false
+                jar = "com/example/runtime-lib/1.0.0/runtime-lib-1.0.0.jar"
+                dependencies = []
+                """);
+    }
+
+    protected static void source(Path projectDir, String path, String content) throws IOException {
+        Path source = projectDir.resolve(path);
+        Files.createDirectories(source.getParent());
+        Files.writeString(source, content);
+    }
+
+    protected static ProjectConfig config(Optional<String> mainClass) {
+        return ProjectConfigs.withDirectDependencies(
+                new ProjectMetadata("demo", "0.1.0", "com.example", currentJavaMajorVersion(), mainClass),
+                Map.of("central", "https://repo.maven.apache.org/maven2"),
+                Map.of(),
+                Map.of(),
+                BuildSettings.defaults());
+    }
+
+    private static void createJarWithEntry(Path jarPath, String entryName) throws IOException {
+        Files.createDirectories(jarPath.getParent());
+        try (JarOutputStream output = new JarOutputStream(Files.newOutputStream(jarPath))) {
+            output.putNextEntry(new JarEntry(entryName));
+            output.write(new byte[] {0});
+            output.closeEntry();
+        }
+    }
+
+    private static String currentJavaMajorVersion() {
+        String version = System.getProperty("java.version");
+        String[] parts = version.split("[._+-]", -1);
+        if (parts.length >= 2 && "1".equals(parts[0])) {
+            return parts[1];
+        }
+        return parts[0];
+    }
+}
